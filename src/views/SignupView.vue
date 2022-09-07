c7c685bf1037cbd5219705e1fd652f61eda37d64
ArielJX <asblackcat@hotmail.com>
2022-09-07 16:09:01 +1200

    layout the signup page

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
+  <div class="signup-form">
+    <h3>Create a personal account</h3>
+    <div class="img-wrapper">
+      <img class="signup-img" src="" alt="">
+    </div>
+    <span>Upload profile photo</span>
+    <div class="signup-box">
+      <div class="signup-box--info">
+        <p>User Name</p>
+        <input type="text">
+      </div>
+      <div class="signup-box--info">
+        <p>Email</p>
+        <input type="text">
+      </div>
+      <div class="signup-box--info">
+        <p>Password</p>
+        <input type="text">
+      </div>
+      <div class="signup-box--info">
+        <p>Confirm Password</p>
+        <input type="text">
+      </div>
+      <button type="button">Create</button>
+    </div>
 
   </div>
 </template>

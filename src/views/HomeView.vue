c4416fe485b54b8896120408d04c575f00c96e22
ArianaLJ <ariana.li2021@gmail.com>
2022-09-20 15:09:42 +1200

    let me go

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@ export default {
     },
     async findproductLists() {
       console.log("filtered products");
-      const response = await fetch("http://localhost:3000/findproducts", {
+      const response = await fetch(`http://localhost:3000/findproducts`, {
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify({
